3dc75534685154694f2cd8710b6a20146dbfc24e
unknown <raulsantiagoramirez1@gmail.com>
2023-11-05 00:25:09 -0500

    dia1

---
+++ numerouno.py
@@ -0,0 +1,28 @@
+
+# * print
+
+print("hola mundo")
+
+# * consulta de tipo de dato (type)
+
+print(type(52))# int
+print(type(1.5))# float
+print(type(True))#  boolean
+print(type("hola mondu"))#  string
+print(type(5 + 2j))# complex number
+
+# * variables 
+
+si = 5 
+print(si)
+
+no = str(si) # str cambia el tipo de dato a string
+print(type(no))
+
+print(len(no)) # len no otorga el largo de la cadena de datos
+# * input
+
+coloca = input("hola coloca algo ")
+print(coloca)
+
+print(input(str)) 
