4adf64617f73f4e2b36ab812cad66bfb36d1219a
unknown <raulsantiagoramirez1@gmail.com>
2023-11-08 18:45:46 -0500

    hola2

---
+++ cambio_de_variables.py
@@ -0,0 +1,14 @@
+a = 4
+b = 6
+print ("A es igual a:",a)
+print ("B es igual a:",b)
+
+# *cambio de variales a, b
+
+print ("Se realiza el cambio de variables*")
+temp = a
+a: float = b 
+b: float = temp 
+
+print ("A es igual a:",float(a))
+print ("B es igual a:",float(b))   
